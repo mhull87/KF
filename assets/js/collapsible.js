d9d977b12daa1747d73c4200d2598669063283b4
Melissa Hull <melissahull87@gmail.com>
2022-05-18 16:50:48 -0500

    change js

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -6,10 +6,10 @@ for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
+        if (content.style.minHeight) {
+            content.style.minHeight = null;
         } else {
-            content.style.maxHeight = content.scrollHeight + "px";
+            content.style.minHeight = content.scrollHeight + "px";
         }
     });
 }
